6c005f93f8cc52094d5239e79522bdc3c56e1ab3
Atik <atikmandhat@gmail.com>
2021-08-24 18:01:05 +0530

    initialize the defination and create second Activity and write jafile

--- app/src/main/java/com/example/tutorial3/MainActivity.java
+++ app/src/main/java/com/example/tutorial3/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.tutorial3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText name, psw;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name=findViewById(R.id.name);
+        psw=findViewById(R.id.psw);
+    }
+
+    public void login(View view) {
+
+        if (name.length() == 0){
+            Toast.makeText(this, "Please Enter Username", Toast.LENGTH_SHORT).show();
+        }
+        else if (psw.length() == 0){
+            Toast.makeText(this, "Please Enter Password", Toast.LENGTH_SHORT).show();
+        }
+        else if (name.equals("admin@gmail.com") && psw.equals("admin"))
+        {
+            Intent intent = new Intent(this,Activity2.class);
+            startActivity(intent);
+            finish();
+        }
+        else
+        {
+            Toast.makeText(this, "Username and password is incorect...", Toast.LENGTH_SHORT).show();
+        }
     }
 }
